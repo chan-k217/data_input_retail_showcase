25201d21cec2f04661b310919ebab00d3dc9752c
chan-k217 <p20chanikya@iima.ac.in>
2024-01-18 17:11:20 +0530

    basics

--- app.py
+++ app.py
@@ -1,12 +1,13 @@
 from flask import Flask, request, jsonify
-app1 = Flask(__name__)
+import os
+app = Flask(__name__)
 
-@app1.route('/')
+@app.route('/')
 def hello_world():
     return 'Hello, World!'
 
 if __name__ == '__main__':
-    app1.run()
+    app.run()
 '''
 # %%
 from jupyter_dash import JupyterDash
