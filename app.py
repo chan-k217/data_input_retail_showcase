6c653378b061381711cb6a781d41ea09ec69b868
chan-k217 <p20chanikya@iima.ac.in>
2024-01-18 18:15:18 +0530

    demo

--- app.py
+++ app.py
@@ -1,3 +1,48 @@
+from dash import Dash, html, dcc
+import plotly.express as px
+import pandas as pd
+
+app = Dash(__name__)
+
+# assume you have a "long-form" data frame
+# see https://plotly.com/python/px-arguments/ for more options
+df = pd.DataFrame({
+    "Fruit": ["Apples", "Oranges", "Bananas", "Apples", "Oranges", "Bananas"],
+    "Amount": [4, 1, 2, 2, 4, 5],
+    "City": ["SF", "SF", "SF", "Montreal", "Montreal", "Montreal"]
+})
+
+fig = px.bar(df, x="Fruit", y="Amount", color="City", barmode="group")
+
+app.layout = html.Div(children=[
+    html.H1(children='Hello Dash'),
+
+    html.Div(children='''
+        Dash: A web application framework for your data.
+    '''),
+
+    dcc.Graph(
+        id='example-graph',
+        figure=fig
+    )
+])
+
+## This is the object that will be passed to gunicorn
+server = app.server
+host_ip = '0.0.0.0'
+if __name__ == '__main__':
+    app.run_server(host = host_ip,port=8000,debug=True)
+
+
+
+
+
+
+
+
+
+
+'''
 from flask import Flask, request, jsonify
 import os
 # app = Flask(__name__)
@@ -656,3 +701,4 @@ host_ip = '0.0.0.0'
 if __name__ == '__main__':
     app.run_server(host = '0.0.0.0',port=8000,debug=True)
 # %%
+'''
